870adcb9234ad73c55f1db666c91b27a4a92a914
Himanshu <bansalhimanshu0296@gmail.com>
2025-07-07 21:50:42 -0400

    Subscription Section done (#15)
    * Images added for static implementation
    
    * Images added for static implementation. Closes #1
    
    * Completed Organization section
    
    * Browsing Job Categories Component
    
    * Working section completed
    
    * static Testimonials section completed
    
    * subscription section completed

---
+++ src/components/LandingPage/Subscribe.tsx
@@ -0,0 +1,17 @@
+import { Button, TextInput } from "@mantine/core"
+
+const Subscribe = () => {
+    return <div className="mt-20 flex items-center bg-mine-shaft-900 mx-20 py-3 rounded-xl justify-around">
+        <div className="text-4xl w-2/5 text-center font-semibold text-mine-shaft-100 mb-3">
+            Never Wants to Miss Any <span className="text-bright-sun-400">Job News?</span>
+        </div>
+        <div className="flex gap-4 rounded-xl bg-mine-shaft-700 px-3 py-2 items-center">
+            <TextInput variant="unstyled"
+                className="[&_input]:text-mine-shaft-100 font-semibold"
+                placeholder="Your@email.com" size="xl" />
+            <Button size="lg"  className="!rounded-lg" color="brightSun.4" variant="filled">Subscribe</Button>
+        </div>
+    </div>
+}
+
+export default Subscribe
