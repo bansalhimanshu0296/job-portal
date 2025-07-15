d500204dade6a455cf1ee29ca243696f2ed792db
Himanshu <bansalhimanshu0296@gmail.com>
2025-07-15 01:45:25 -0400

    Static FInd Job Page (#18)
    * Images added for static implementation
    
    * Images added for static implementation. Closes #1
    
    * Completed Organization section
    
    * Browsing Job Categories Component
    
    * Working section completed
    
    * static Testimonials section completed
    
    * subscription section completed
    
    * footer done
    
    * Moving Header and footer to common element
    
    * Static Search bar completed
    
    * remove jasmine
    
    * completed SLider for Salary and different Search bars
    
    * Static FInd Job Page DOne

---
+++ src/components/FindJobs/Jobs.tsx
@@ -0,0 +1,20 @@
+import { jobList } from "../../data/JobsData";
+import JobCard from "./JobCard";
+import Sort from "./Sort";
+
+const Jobs = () => {
+    return <div className="p-5">
+        <div className="flex justify-between">
+            <div className="text-2xl font-semibold">Recommended Jobs</div>
+            <Sort />
+        </div>
+        <div className="mt-10 flex flex-wrap gap-10">
+            {
+                jobList.map((job, index) => <JobCard key={index} {...job} />)
+            }
+        </div>
+
+    </div>
+}
+
+export default Jobs;
---
+++ src/components/FindJobs/MultiInput.tsx
@@ -0,0 +1,111 @@
+import { Checkbox, Combobox, Group, Input, Pill, PillsInput, useCombobox } from "@mantine/core";
+import { IconSelector, type Icon, type IconProps } from "@tabler/icons-react";
+import { useState, type ForwardRefExoticComponent, type RefAttributes } from "react";
+
+
+interface props {
+    icon: ForwardRefExoticComponent<IconProps & RefAttributes<Icon>>;
+    options: string[];
+    title: string;
+}
+
+const MultiInput = (props: props) => {
+    const combobox = useCombobox({
+        onDropdownClose: () => combobox.resetSelectedOption(),
+        onDropdownOpen: () => combobox.updateSelectedOptionIndex('active')
+    });
+    const [search, setSearch] = useState('');
+    const [value, setValue] = useState<string[]>([]);
+    const [data, setData] = useState<string[]>(props.options);
+
+    const exactOptionMatch = data.some((item) => item === search);
+
+    const handleValueSelect = (val: string) => {
+        setSearch('');
+
+        if (val === '$create') {
+            setData((current) => [...current, search]);
+            setValue((current) => [...current, search]);
+        } else {
+            setValue((current) => current.includes(val) ? current.filter((v) => v != val) : [...current, val]);
+        }
+    };
+
+    const handleValueRemove = (val: string) => setValue((current) => current.filter((v) => v != val));
+
+    const values = value.slice(0, 1)
+    .map((item) => (
+        <Pill key={item} withRemoveButton onRemove={() => handleValueRemove(item)}>
+            {item}
+        </Pill>
+    ));
+
+    const options = data.
+        filter((item) => item.toLowerCase().includes(search.trim().toLowerCase()))
+        .map((item) => (
+            <Combobox.Option value={item} key={item} active={value.includes(item)}>
+                <Group gap="sm">
+                    <Checkbox size="xs" color="brightSun.4"
+                        checked={value.includes(item)}
+                        onChange={() => {}}
+                        aria-hidden
+                        tabIndex={-1}
+                        style={{ pointerEvents: 'none' }}
+                    />
+                    <span className="text-mine-shaft-300">{item}</span>
+                </Group>
+            </Combobox.Option>
+        ));
+    
+    return (
+        <Combobox store={combobox} onOptionSubmit={handleValueSelect} withinPortal={false}>
+            <Combobox.DropdownTarget>
+                <PillsInput variant="unstyled" rightSection={<IconSelector/>}
+                    onClick={() => combobox.toggleDropdown()} leftSection={
+                    <div
+                        className="text-bright-sun-400 p-1 bg-mine-shaft-900
+                        rounded-full mr-2">
+                        <props.icon />
+                    </div>}>
+                    <Pill.Group>
+                        {
+                            value.length > 0 ? (
+                                <>
+                                    {values}
+                                    {value.length > 1 && (
+                                        <Pill>+{value.length - 1} more</Pill>
+                                    )}
+                                </>
+                            ) : (
+                                    <Input.Placeholder className="!text-mine-shaft-200">{props.title}</Input.Placeholder>
+                            )
+                        }    
+                    </Pill.Group>
+                </PillsInput>
+            </Combobox.DropdownTarget>
+            <Combobox.Dropdown>
+                <Combobox.Search
+                    value={search}
+                    onChange={(event) => setSearch(event.currentTarget.value)}
+                    placeholder="Search Groceries"
+                />
+                <Combobox.Options>
+                    {options}
+                    {!exactOptionMatch && search.trim().length > 0 && (
+                        <Combobox.Option value="$create">
+                            + Create {search}
+                        </Combobox.Option>
+                    )}
+
+                    {exactOptionMatch && search.trim().length > 0 && options.length === 0 && (
+                        <Combobox.Empty>
+                            Nothing Found
+                        </Combobox.Empty>
+                    )}
+                </Combobox.Options>
+            </Combobox.Dropdown>
+        </Combobox>
+    )
+}
+
+export default MultiInput
